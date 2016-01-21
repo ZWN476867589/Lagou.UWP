cc717b9c282722d0912288ece359c0c5790d1e6f
xlingfairy <gruan@asnum.com>
2016-01-21 23:57:49 +0800

    Missing File

---
+++ Lagou.UWP/ViewModels/SearchBarViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Lagou.UWP.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lagou.UWP.ViewModels {
+
+    [Regist(InstanceMode.PreRequest)]
+    public class SearchBarViewModel : BaseVM {
+    }
+}
